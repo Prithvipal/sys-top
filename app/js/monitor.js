f62f6d82c63bb958bc40a6f3de3e48a80503fc29
Prithvipal Singh <prithvirathore99@gmail.com>
2020-10-18 21:52:16 +0530

    dynamic system stats

--- app/js/monitor.js
+++ app/js/monitor.js
@@ -4,6 +4,18 @@ const cpu = osu.cpu
 const mem = osu.mem
 const os = osu.os
 
+setInterval(() =>{
+    cpu.usage().then(info =>{
+        document.getElementById("cpu-usage").innerText = info + "%"
+    })
+
+    cpu.free().then(info =>{
+        document.getElementById("cpu-free").innerText = info +"%"
+    })
+
+    document.getElementById("sys-uptime").innerText = secondsToDhms(os.uptime())
+}, 2000)
+
 // Set model
 document.getElementById("cpu-model").innerText = cpu.model()
 
@@ -18,3 +30,12 @@ document.getElementById("os").innerText = `${os.type()} ${os.arch()}`
 mem.info().then(info =>{
     document.getElementById("mem-total").innerText = info.totalMemMb
 })
+
+function secondsToDhms(seconds){
+    seconds = +seconds
+    const d = Math.floor(seconds / (3600 * 24))
+    const h = Math.floor((seconds % (3600 * 24))/3600)
+    const m = Math.floor((seconds % (3600))/60)
+    const s = Math.floor(seconds % 60)
+    return `${d}d, ${h}h, ${m}m ${s}s`
+}
