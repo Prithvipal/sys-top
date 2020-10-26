b3b69282b8a83727bdc71d53185e6e920e0cd0e0
Prithvipal Singh <prithvirathore99@gmail.com>
2020-10-26 08:36:48 +0530

    using settings in monitor

--- app/js/monitor.js
+++ app/js/monitor.js
@@ -1,11 +1,18 @@
 const path = require("path")
+const {ipcRenderer} = require("electron")
 const osu = require("node-os-utils")
 const cpu = osu.cpu
 const mem = osu.mem
 const os = osu.os
 
-let cpuOverload = 5
-let alertFrequency = 1
+let cpuOverload
+let alertFrequency
+
+ipcRenderer.on("settings:get", (e, settings)=> {
+    cpuOverload = +settings.cpuOverload
+    alertFrequency = +settings.alertFrequency
+})
+
 
 
 
